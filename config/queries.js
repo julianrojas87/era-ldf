1ac454366d2dfac3ddcfbb3db8b3db6348c99863
Julian Rojas <julian.rojas87@gmail.com>
2020-11-25 01:52:25 +0100

    Go back to specific query for vehicles

--- config/queries.js
+++ config/queries.js
@@ -311,9 +311,20 @@ export const vehicleInstances = {
         accept: 'text/turtle',
         query: `
         PREFIX era: <http://era.europa.eu/ns#>
-        CONSTRUCT WHERE {
+        CONSTRUCT {
             ?s a era:Vehicle;
-                ?p ?o.
+                era:vehicleNumber ?vn;
+                era:vehicleSeries ?vs;
+                era:vehicleType ?vt;
+                era:operationalRestriction ?or;
+                era:quieterRoutesExemptedCountry ?ec.
+        } WHERE {
+            ?s a era:Vehicle;
+                era:vehicleNumber ?vn.
+            OPTIONAL { ?s era:vehicleSeries ?vs }
+            OPTIONAL { ?s era:vehicleType ?vt }
+            OPTIONAL { ?s era:operationalRestriction ?or }
+            OPTIONAL { ?s era:quieterRoutesExemptedCountry ?ec }
         }
         `
     }
