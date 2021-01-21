6fbe91304605d36956e88c87a3886448c6087cf4
Julian Rojas <julian.rojas87@gmail.com>
2021-01-21 13:28:18 +0100

    Adjust to work with Virtuoso

--- config/config.js
+++ config/config.js
@@ -4,7 +4,8 @@
  */
 
 export const config = {
-    sparql: 'http://era.ilabt.imec.be/repositories/ERA-KG', // GraphDB
-    //sparql: 'https://test-linked.ec-dataplatform.eu/sparql', // Virtuoso
-    sparqlTileBaseURI: 'http://era.ilabt.imec.be/sparql-tiles'
+    //sparql: 'http://era.ilabt.imec.be/repositories/ERA-KG', // GraphDB
+    sparql: 'https://test-linked.ec-dataplatform.eu/sparql', // Virtuoso
+    sparqlTileBaseURI: 'http://era.ilabt.imec.be/sparql-tiles',
+    namedGraph: 'urn:era-kg-v1.1.2.ttl' // Needed for Virtuoso
 };
